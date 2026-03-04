f572b9e6db52a658772d113906664f0cd3bb97d6
lovyverma231-del <lovyverma231@gmail.com>
2026-03-05 05:01:08 +0530

    Update server.js
    updated server port

--- backend/server.js
+++ backend/server.js
@@ -31,8 +31,10 @@ app.use('/api/sheets',   require('./routes/sheets'));
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('✅ MongoDB connected');
-    app.listen(process.env.PORT || 5000, () =>
-      console.log('🚀 Server running on port', process.env.PORT || 5000)
-    );
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+    });
   })
   .catch(err => console.error('MongoDB error:', err));
